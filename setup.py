4eef3ae12bc5b3cba93f18dcb89c596a00d4dd3d
Jake Barber <jake.b.dev@gmail.com>
2018-06-27 22:41:20 +1000

    release 0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="review_ipsum",
-    version="0.0.1",
+    version="0.0.2",
     author="Jake Barber",
     author_email="jake.b.dev@gmail.com",
     description="Review Themed Lorem Ipsum",
